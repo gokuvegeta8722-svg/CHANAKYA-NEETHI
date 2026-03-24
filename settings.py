4219c013797ebac40c2f1b121a8775b8101dc63b
gokuvegeta8722-svg <gokuvegeta8722@gmail.com>
2026-03-24 21:44:47 +0530

    Create settings.py with game configuration settings

---
+++ settings.py
@@ -0,0 +1,24 @@
+# Game Configuration Settings
+
+## Resolution
+resolution = {
+    'width': 1920,
+    'height': 1080
+}
+
+## FPS
+fps = 60
+
+## Paths
+paths = {
+    'assets': 'assets/',
+    'config': 'config/',
+    'scripts': 'scripts/'
+}
+
+## Game Constants
+constants = {
+    'gravity': 9.8,
+    'max_players': 4,
+    'game_name': 'CHANAKYA NEETHI'
+}
